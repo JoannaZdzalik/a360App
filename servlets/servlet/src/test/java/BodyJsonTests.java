a59d32264538860d3e9e39b09f08f73ee0d5530e
LukaszStepien <lukasz.stepien@avenga.com>
2020-02-13 15:30:53 +0100

    overall brush up

--- servlets/servlet/src/test/java/BodyJsonTests.java
+++ servlets/servlet/src/test/java/BodyJsonTests.java
@@ -1,60 +1,29 @@
-import com.avenga.a360.dto.SessionDto;
-import com.avenga.a360.model.Session;
-import com.avenga.a360.model.response.Status;
-import com.avenga.a360.service.SessionService;
-import org.apache.http.HttpStatus;
-
+import com.avenga.a360.dao.QuestionDao;
+import com.avenga.a360.dto.AnswerDto;
+import com.avenga.a360.dto.QuestionDto;
+import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-
-
-import javax.inject.Inject;
-
-import static org.mockito.Mockito.when;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import java.util.HashMap;
 import java.util.Map;
 
-//public class BodyJsonTests {
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
+public class BodyJsonTests {
+    @Test
+    public void aCarObjectGoesIntoTheGarage() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("answerText", "odpowiedz");
+        jsonAsMap.put("questionId", "1");
+        jsonAsMap.put("participantUId", "12345");
 
-
-/*    @Test
-    public void givenMovieId_whenMakingGetRequestToMovieEndpoint_thenReturnMovie() {
-
-Session session = new Session();
-
-
-
-        when(sessionService.createSession(any(SessionDto.class),any())).thenReturn(any(Status.class));
-
-        get(uri + "/movie/" + testMovie.getId()).then()
-                .assertThat()
-                .statusCode(HttpStatus.OK.value())
-                .body("id", equalTo(testMovie.getId()))
-                .body("name", equalTo(testMovie.getName()))
-                .body("synopsis", notNullValue());
-    }*/
-/*@Test
-public void givenMovie_whenMakingPostRequestToMovieEndpoint_thenCorrect() {
-    Map<String, String> request = new HashMap<>();
-    request.put("id", "11");
-    request.put("name", "movie1");
-    request.put("synopsis", "summary1");
-
-    int movieId = given().contentType("application/json")
-            .body(request)
-            .when()
-            .post(uri + "/movie")
-            .then()
-            .assertThat()
-            .statusCode(HttpStatus.CREATED.value())
-            .extract()
-            .path("id");
-    assertThat(movieId).isEqualTo(11);
-
-}*/
+        given().
+                contentType(ContentType.JSON).
+                body(jsonAsMap).
+                when().
+                post("http://127.0.0.1:8080/servlet/A360/answers/create").
+                then().
+                statusCode(200);
+    }
+}
