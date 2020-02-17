a6ed8f0209a1f800c1b3b957ee308b01da0715ee
PiotrKapusta <piotr.kapusta@avenga.com>
2020-02-17 16:34:19 +0100

    Add view to session

--- servlets/servlet/src/main/webapp/A360/components/session/sessionController.js
+++ servlets/servlet/src/main/webapp/A360/components/session/sessionController.js
@@ -1,8 +1,16 @@
 (function() {
     "use strict";
     angular.module('a360').controller('SessionController', SessionController);
+
     SessionController.$inject = ['$scope'];
+
+
     function SessionController($scope) {
         $scope.title = "Create - Avenga 360 feedback session";
+
+
+
+
     }
+
 })();
