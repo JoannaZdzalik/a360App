eaab21de2b818b888224bfb49c6b782e1952b25b
PiotrKapusta <piotr.kapusta@avenga.com>
2020-02-20 11:07:48 +0100

    refactor session vol 2

--- servlets/servlet/src/main/webapp/A360/components/session/sessionController.js
+++ servlets/servlet/src/main/webapp/A360/components/session/sessionController.js
@@ -1,102 +1,95 @@
-(function() {
+(function () {
     "use strict";
     angular.module('a360').controller('SessionController', SessionController);
 
-    SessionController.$inject = ['$scope','SessionService'];
+    SessionController.$inject = ['$scope', 'SessionService', '$filter'];
 
 
-    function SessionController($scope, SessionService) {
+    function SessionController($scope, SessionService, $filter) {
 
 
-        $scope.init = function(){
+        $scope.init = function () {
             $scope.title = "Create - Avenga 360 feedback session";
             $scope.participants = [];
-            $scope.participantEmail = "";
             $scope.sessionForm = "";
             $scope.sessionName = "";
             $scope.inputEmail = "";
-            $scope.showAlert = false;
-            $scope.endDate = new Date();
-            $scope.emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            $scope.showAlertPositive = false;
+            $scope.showAlertError = false;
+            $scope.endDate = "";
+            $scope.showAlert = "";
+            $scope.cleanFields();
+
 
         }
+        $scope.emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         $scope.endDateOptions = {
-            dateDisabled: function(data, mode) {
+            dateDisabled: function (data, mode) {
                 var date = data.date;
                 var mode = data.mode;
                 return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
             },
             formatYear: 'yy',
             maxDate: new Date(2090, 5, 22),
-            minDate: new Date(),
+            minDate: addDaysToDate(new Date(), 1),
             startingDay: 1
         };
+
+        function addDaysToDate(date, days) {
+            var newDate = new Date(date);
+            newDate.setDate(newDate.getDate() + days)
+            return newDate
+
+        }
+
         $scope.endDatePopup = {
             opened: false
         };
-        $scope.openEndDatePopup = function() {
+        $scope.openEndDatePopup = function () {
             $scope.endDatePopup.opened = true;
         };
 
-        $scope.addParticipant = function() {
-            if($scope.inputEmail==0 || $scope.participants.includes($scope.inputEmail)){
+        $scope.addParticipant = function () {
+            if ($scope.inputEmail == 0 || $scope.participants.includes($scope.inputEmail)) {
                 $scope.inputEmail = '';
-            }else{
-                $scope.participants.push({"email":$scope.inputEmail});
+            } else {
+                $scope.participants.push({"email": $scope.inputEmail});
                 $scope.inputEmail = '';
             }
 
 
-
         }
-        $scope.removeParticipant = function($index) {
-            $scope.participants.splice($index,1);
+        $scope.removeParticipant = function ($index) {
+            $scope.participants.splice($index, 1);
         }
         $scope.canSendSession = function () {
             return $scope.participants.length === 0 || $scope.sessionForm.sessionName.$invalid || $scope.sessionForm.endDate.$invalid
         }
-        $scope.cleanFields = function(){
-            $scope.sessionName ="";
-            $scope.endDate = new Date();
+        $scope.cleanFields = function () {
+            $scope.sessionName = "";
+            $scope.endDate = "";
             $scope.participants = []
+            $scope.inputEmail = "";
         }
 
 
-
-
-        $scope.dateParser = function () {
-            let year = $scope.endDate.getFullYear();
-            let month = $scope.endDate.getMonth()+1;
-            let day = $scope.endDate.getDate();
-            let hour = $scope.endDate.getHours();
-            let min = $scope.endDate.getMinutes();
-            if ($scope.endDate.getMonth()<10) {
-                month = `0${$scope.endDate.getMonth()+1}`
-            }
-            if ($scope.endDate.getDate()<10) {
-                day = `0${$scope.endDate.getDate()}`
-            }
-            if ($scope.endDate.getMinutes()<10) {
-                min = `0${$scope.endDate.getMinutes()}`
-            }
-            return `${year}-${month}-${day} ${hour}:${min}`
+        $scope.formatDate = function () {
+            var convertedEndDate = $filter('date')(new Date($scope.endDate), 'yyyy-MM-dd hh:mm');
+            return convertedEndDate
         };
-        $scope.createSession = function() {
-            SessionService.send( $scope.sessionName, $scope.dateParser(), $scope.participants).then(function(data) {
+        $scope.createSession = function () {
+            SessionService.send($scope.sessionName, $scope.formatDate(), $scope.participants).then(function (data) {
                 $scope.cleanFields();
-                $scope.showAlert = true;
-                console.log('success ' + data);
-            }, function(response) {
-                alert(response);
+                $scope.showAlert = "Success!";
+            },
+                function (response) {
+                $scope.showAlert = 'Error!';
             });
         };
 
 
-
     }
-
-
 
 
 })();
